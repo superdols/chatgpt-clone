54fac37ebf9888a569e664f00ffcd594849ab020
Kihun Park <superdols37@gmail.com>
2025-09-21 16:11:43 +0900

    conversation history

--- main.py
+++ main.py
@@ -1,6 +1,61 @@
-def main():
-    print("Hello from chatgpt-clone!")
+import dotenv
+dotenv.load_dotenv()
+import asyncio
+import streamlit as st
+from agents import Agent, Runner, SQLiteSession
 
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="ChatGPT Clone",
+        instructions="""
+        You are a helpful assistant.
+        """,
+    )
+    
+agent = st.session_state["agent"]
 
-if __name__ == "__main__":
-    main()
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history",
+        "chat-gpt-clone-memory.db"
+    )
+
+session = st.session_state["session"]
+
+async def paint_history():
+    messages = await session.get_items()
+
+    for message in messages:
+        with st.chat_message(message["role"]):
+            if message["role"] == "user":
+                st.write(message["content"])
+            else:
+                if message["type"] == "message":
+                    st.write(message["content"][0]["text"])
+
+asyncio.run(paint_history())
+
+async def run_agent(message):
+    with st.chat_message("ai"):
+        text_placeholder = st.empty()
+        response = ""
+        stream = Runner.run_streamed(agent, message, session=session)
+
+        async for event in stream.stream_events():
+            if event.type == "raw_response_event":
+                if event.data.type == "response.output_text.delta":
+                        response += event.data.delta
+                        text_placeholder.write(response)
+
+prompt = st.chat_input("Write a message for your assistant.")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
+    asyncio.run(run_agent(prompt))
+
+with st.sidebar:
+    reset = st.button("Reset memory")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
